7d04dbcc585d0e5dc38703ad2a8bc35dca666448
MineRickStar <Patrick.zann97@gmail.com>
2020-10-08 21:31:47 +0200

    update
    removed uneccessary classes

--- src/start/Category.java
+++ src/start/Category.java
@@ -1,13 +1,30 @@
 package start;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 public class Category implements Serializable {
 
@@ -19,14 +36,116 @@ public class Category implements Serializable {
 		return Category.allCategories;
 	}
 
+	public static Category newCategory(JFrame parent) {
+		return Category.display(parent, null);
+	}
+
+	public static void editCategories(JFrame parent) {
+		JDialog dialog = new JDialog(parent, "Kategorien bearbeiten", true);
+		dialog.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(5, 5, 5, 5);
+		DefaultListModel<Category> listModel = new DefaultListModel<>();
+		JList<Category> categoryList = new JList<>(listModel);
+		categoryList.setFixedCellWidth(200);
+		categoryList.setFixedCellHeight(20);
+		Category.allCategories.forEach(listModel::addElement);
+
+		gbc.gridx++;
+		gbc.gridy++;
+		dialog.add(new JScrollPane(categoryList), gbc);
+		JPanel buttonPanel = new JPanel();
+		JButton edit = new JButton("bearbeiten");
+		edit.addActionListener(e -> {
+			if (categoryList.getSelectedValue() != null) {
+				Category.display(parent, categoryList.getSelectedValue());
+			}
+		});
+		buttonPanel.add(edit);
+		JButton delete = new JButton("Löschen");
+		delete.addActionListener(e -> {
+			if (categoryList.getSelectedValue() != null) {
+				if (JOptionPane.showConfirmDialog(dialog, "Wirklich löschen?", "Löschen",
+						JOptionPane.OK_CANCEL_OPTION) != JOptionPane.OK_OPTION) {
+					return;
+				}
+				Category c = categoryList.getSelectedValue();
+				Category.allCategories.remove(c);
+				listModel.removeElement(c);
+				for (Iterator<Category> iterator = Category.allCategories.iterator(); iterator.hasNext();) {
+					Category category = iterator.next();
+					if (c.equals(category.getParentCategory())) {
+						listModel.removeElement(category);
+						iterator.remove();
+					}
+				}
+			}
+		});
+		buttonPanel.add(delete);
+		gbc.gridy++;
+		dialog.add(buttonPanel, gbc);
+		dialog.pack();
+		dialog.setResizable(false);
+		dialog.setLocationRelativeTo(parent);
+		dialog.setVisible(true);
+	}
+
+	private static Category display(JFrame parent, Category category) {
+		JDialog dialog = new JDialog(parent, "Neue Kategorie", true);
+		dialog.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(5, 5, 5, 5);
+		JLabel parentLabel = new JLabel("Überkategorie");
+		gbc.gridx++;
+		gbc.gridy++;
+		dialog.add(parentLabel, gbc);
+		JComboBox<Category> parentCategory = new JComboBox<>(Category.allCategories.toArray(new Category[0]));
+		parentCategory.insertItemAt(null, 0);
+		parentCategory.setSelectedIndex(0);
+		if (category != null) {
+			parentCategory.setSelectedItem(category.getParentCategory());
+		}
+		gbc.gridx++;
+		dialog.add(parentCategory, gbc);
+		JLabel categoryLabel = new JLabel("Kategorie");
+		gbc.gridy++;
+		gbc.gridx = 0;
+		dialog.add(categoryLabel, gbc);
+		JTextField textField = new JTextField(10);
+		if (category != null) {
+			textField.setText(category.getName());
+		}
+		gbc.gridx++;
+		dialog.add(textField, gbc);
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(e -> dialog.dispose());
+		gbc.gridy++;
+		gbc.gridx = 0;
+		gbc.gridwidth = 2;
+		dialog.add(okButton, gbc);
+
+		dialog.pack();
+		dialog.setResizable(false);
+		dialog.setLocationRelativeTo(parent);
+		dialog.setVisible(true);
+
+		if (textField.getText().isEmpty()) {
+			return null;
+		}
+
+		return new Category((Category) parentCategory.getSelectedItem(), textField.getText());
+	}
+
 	static {
 		Runtime.getRuntime().addShutdownHook(new Thread(Category::write));
 	}
 
 	public static void read() {
 		try (ObjectInputStream ois = new ObjectInputStream(
-				new FileInputStream(new File(Start.location, "Categorien")))) {
+				new FileInputStream(new File(Start.location, "Alle Kategorien.category")))) {
 			Category.allCategories = (ArrayList<Category>) ois.readObject();
+		} catch (FileNotFoundException e) {
+			// thats ok
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -34,23 +153,58 @@ public class Category implements Serializable {
 
 	private static void write() {
 		try (ObjectOutputStream oos = new ObjectOutputStream(
-				new FileOutputStream(new File(Start.location, "Categorien")))) {
+				new FileOutputStream(new File(Start.location, "Alle Kategorien.category")))) {
 			oos.writeObject(Category.allCategories);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	Category parentCategory;
+
 	String name;
 
-	public Category(String name) {
+	public Category(Category parentCategory, String name) {
+		this.parentCategory = parentCategory;
 		this.name = name;
 		Category.allCategories.add(this);
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getParentCategory() {
+		return this.parentCategory;
+	}
+
+	public void setParentCategory(Category parentCategory) {
+		this.parentCategory = parentCategory;
 	}
 
 	@Override
 	public String toString() {
 		return this.name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o instanceof Category) {
+			boolean nameEqual = this.name.equals(((Category) o).name);
+			if (this.parentCategory != null) {
+				return nameEqual && this.parentCategory.equals(((Category) o).parentCategory);
+			} else {
+				return nameEqual;
+			}
+		}
+		return false;
 	}
 
 }
--- src/start/Start.java
+++ src/start/Start.java
@@ -2,8 +2,11 @@ package start;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.nio.file.Files;
@@ -12,14 +15,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import javax.swing.DefaultListModel;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.KeyStroke;
 import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
 
@@ -29,21 +34,21 @@ public class Start extends JFrame {
 
 	public static final File location = new File(System.getProperty("user.dir"), "Mama Abrechnung");
 
-	public static final File receiptsFile = new File(Start.location, "Kassenzettel");
+	public static final File allCategoriesFile = new File(Start.location, "Kategorien");
 
-	JList<Receipt> list;
+	JList<Expense> list;
 
-	DefaultListModel<Receipt> listModel;
+	DefaultListModel<Expense> listModel;
 
-	ArrayList<Receipt> receipts;
+	public ArrayList<Expense> expenses;
 
 	public Start() {
 		super("Mamas Abrechnungsprogramm");
 		this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		this.receipts = new ArrayList<>();
+		this.expenses = new ArrayList<>();
 		this.listModel = new DefaultListModel<>();
 		this.list = new JList<>(this.listModel);
-		Start.receiptsFile.mkdirs();
+		Start.allCategoriesFile.mkdirs();
 		this.read();
 		this.setSize(400, 400);
 		this.setResizable(false);
@@ -54,10 +59,12 @@ public class Start extends JFrame {
 
 		this.list.setCellRenderer((thisList, value, index, isSelected, cellHasFocus) -> {
 			JPanel panel = new JPanel(new GridLayout(1, 0, 10, 10));
-			JLabel label = new JLabel("Datum: " + value.getLocaleDateTimeString());
+			JLabel label = new JLabel("Datum: " + value.getDateTimeString());
 			panel.add(label);
-			JLabel label2 = new JLabel("Summe: " + new DecimalFormat("#.00").format(value.getSumme()) + "€");
+			JLabel label2 = new JLabel("Summe: " + new DecimalFormat("#.00").format(value.getValue()) + "€");
 			panel.add(label2);
+			JLabel label3 = new JLabel("Kategorie: " + value.getCategory());
+			panel.add(label3);
 
 			if (isSelected) {
 				panel.setBackground(Color.GRAY);
@@ -75,57 +82,65 @@ public class Start extends JFrame {
 	private void addMenu() {
 		JMenuBar menuBar = new JMenuBar();
 
-		JButton newReceipt = new JButton("Neu");
-//		newReceipt.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
-		newReceipt.addActionListener(e -> {
-			Receipt receipt = Receipt.newReceipt(this);
-			if (receipt != null) {
-				this.receipts.add(receipt);
-				this.listModel.addElement(receipt);
+		JMenu expenseMenu = new JMenu("Ausgabe");
+
+		JMenuItem newExpense = new JMenuItem("Neue Ausgabe");
+		newExpense.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+		newExpense.addActionListener(e -> {
+			Expense expense = Expense.newExpense(this);
+			if (expense != null) {
+				this.expenses.add(expense);
+				this.listModel.addElement(expense);
 				this.revalidate();
 			}
 		});
 
-		JButton edit = new JButton("Bearbeiten");
-//		edit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_DOWN_MASK));
-		edit.addActionListener(e -> {
-			if (this.list.getSelectedValue() == null) {
-				return;
-			}
-			Receipt.editReceipt(this.list.getSelectedValue());
-		});
-
-		JButton delete = new JButton("Löschen");
-//		delete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
+		JMenuItem delete = new JMenuItem("Ausgabe Löschen");
+		delete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
 		delete.addActionListener(e -> {
-			Receipt r = this.list.getSelectedValue();
+			Expense r = this.list.getSelectedValue();
 			if (r != null) {
 				if (JOptionPane.showConfirmDialog(Start.this, "Datensatz wirklich löschen?", "Löschen?",
 						JOptionPane.YES_NO_OPTION) == 0) {
 					this.listModel.removeElement(r);
-					this.receipts.remove(r);
-					new File(Start.receiptsFile, r.getID() + ".bon").delete();
+					this.expenses.remove(r);
+					new File(Start.allCategoriesFile, r.getID() + ".exp").delete();
 				}
 			}
-
 		});
 
-		menuBar.add(newReceipt);
-		menuBar.add(edit);
-		menuBar.add(delete);
+		JMenu category = new JMenu("Kategorien");
+
+		JMenuItem newCategory = new JMenuItem("Neue Kategorie");
+		newCategory.addActionListener(e -> Category.newCategory(Start.this));
+
+		JMenuItem editCategories = new JMenuItem("Kategorien bearbeiten");
+		editCategories.addActionListener(e -> Category.editCategories(Start.this));
+
+		expenseMenu.add(newExpense);
+		expenseMenu.add(delete);
+
+		category.add(newCategory);
+		category.add(editCategories);
+
+		menuBar.add(expenseMenu);
+		menuBar.add(category);
+
 		this.setJMenuBar(menuBar);
 	}
 
 	private void read() {
 		Category.read();
-		File[] files = Start.receiptsFile.listFiles();
+		File[] files = Start.allCategoriesFile.listFiles();
 		Arrays.asList(files).forEach(file -> {
 			try {
 				byte[] bytes = Files.readAllBytes(file.toPath());
 				ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes));
-				Receipt r = (Receipt) ois.readObject();
-				this.receipts.add(r);
-				this.listModel.addElement(r);
+				Expense e = (Expense) ois.readObject();
+				this.expenses.add(e);
+				this.listModel.addElement(e);
+			} catch (FileNotFoundException e) {
+				// thats ok
 			} catch (IOException | ClassNotFoundException e) {
 				e.printStackTrace();
 			}
